4aa54bddd67ff0d3edb8141bc08371d5c90ec13a
Jovidon <you@example.comjova_94@inbox.ru>
2019-07-30 19:12:29 -0700

    File is changed

---
+++ src/test/java/Tests/VytrackScript.java
@@ -0,0 +1,11 @@
+package Tests;
+
+import java.time.LocalDate;
+
+public class VytrackScript{
+
+
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
